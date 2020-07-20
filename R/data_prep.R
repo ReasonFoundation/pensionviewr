ef1301d280c9141855f21eee0340628f9e231e37
Anil Niraula <niraulaanil@gmail.com>
2020-07-20 18:51:01 -0400

    Added pullSourceData() to data_prep

--- R/data_prep.R
+++ R/data_prep.R
@@ -103,6 +103,51 @@ pullData <-
       janitor::clean_names()
   }
 
+#' Pull the data sources for a specified plan from the Reason pension database.
+#'
+#' @param pl A datafram containing the list of plan names, states, and ids in the form produced by the planList() function
+#' @param plan_name A string enclosed in quotation marks containing a plan name as it is listed in the Reason pension database.
+#' @return A wide data frame with each year as a row and variables as columns.
+#' @export
+#' @importFrom rlang .data
+#' @examples
+#' \dontrun{
+#' pullSourceData(pl)
+devtools::test()
+#' pullSourceData(pl, "Kansas Public Employees' Retirement System")
+#' }
+pullSourceData <- 
+  function(pl, plan_name) {
+    #dw <- get("dw")
+    con <- RPostgres::dbConnect(
+      RPostgres::Postgres(),
+      dbname = trimws(dw$path),
+      host = dw$hostname,
+      port = dw$port,
+      user = dw$username,
+      password = dw$password,
+      sslmode = "require"
+    )
+    # define the query to retrieve the plan data
+    #Moved plan_id up to use it in the query below.
+    plan_id <- pl$id[pl$display_name == plan_name]
+    query <- paste("select * from pull_plan_data(",plan_id,")")
+    
+    result <- RPostgres::dbSendQuery(con, query)
+    #RPostgres::dbBind(result, list(1))
+    all_data <- RPostgres::dbFetch(result) %>%
+      janitor::clean_names()
+    RPostgres::dbClearResult(result)
+    RPostgres::dbDisconnect(con)
+    
+    all_data %>%
+      dplyr::group_by_at(dplyr::vars(-.data$attribute_value)) %>%  # group by everything other than the value column.
+      dplyr::mutate(row_id = 1:dplyr::n()) %>%
+      dplyr::ungroup() %>%  # build group index
+      tidyr::spread(.data$attribute_name, .data$attribute_value, convert = TRUE) %>%    # spread
+      dplyr::select(-.data$row_id) %>%  # drop the index
+      janitor::clean_names()
+  }
 
 #' Load the data for a specified plan from an Excel file.
 #'
