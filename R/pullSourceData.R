d2a7dea5e2f19804659b15cdee2091c8f1a3d70e
Anil Niraula <niraulaanil@gmail.com>
2020-07-20 19:10:09 -0400

    Updated pullSourceData function

--- R/pullSourceData.R
+++ R/pullSourceData.R
@@ -1,32 +1,27 @@
-pullSourceData <- 
-  function(pl, plan_name) {
-    #dw <- get("dw")
-    con <- RPostgres::dbConnect(
-      RPostgres::Postgres(),
-      dbname = trimws(dw$path),
-      host = dw$hostname,
-      port = dw$port,
-      user = dw$username,
-      password = dw$password,
-      sslmode = "require"
-    )
-    # define the query to retrieve the plan data
-    #Moved plan_id up to use it in the query below.
-    plan_id <- pl$id[pl$display_name == plan_name]
-    query <- paste("select * from pull_plan_data(",plan_id,")")
-    
-    result <- RPostgres::dbSendQuery(con, query)
-    #RPostgres::dbBind(result, list(1))
-    all_data <- RPostgres::dbFetch(result) %>%
-      janitor::clean_names()
-    RPostgres::dbClearResult(result)
-    RPostgres::dbDisconnect(con)
-    
-    all_data %>%
-      dplyr::group_by_at(dplyr::vars(-.data$attribute_value)) %>%  # group by everything other than the value column.
-      dplyr::mutate(row_id = 1:dplyr::n()) %>%
-      dplyr::ungroup() %>%  # build group index
-      tidyr::spread(.data$attribute_name, .data$attribute_value, convert = TRUE) %>%    # spread
-      dplyr::select(-.data$row_id) %>%  # drop the index
-      janitor::clean_names()
-  }
+pullSourceData <- function(plan_name){
+  con <- RPostgres::dbConnect(
+    RPostgres::Postgres(),
+    dbname = "d629vjn37pbl3l",
+    host = "ec2-3-209-200-73.compute-1.amazonaws.com",
+    port = 5432,
+    user = "reason_readonly",
+    password = "p88088bd28ea68027ee96c65996f7ea3b56db0e27d7c9928c05edc6c23ef2bc27",
+    sslmode = "require")
+  # define the query to retrieve the plan data
+  
+  if(str_count(plan_name)<6){
+    query <- paste("select * from pull_data_state_only()
+where year > '2001'
+and attribute_name in ('1 Year Investment Return Percentage',
+'1 Year Investment Return Percentage',
+'Investment Return Assumption for GASB Reporting',
+'Actuarially Accrued Liabilities Dollar',
+'Total Normal Cost Percentage',
+'Covered Payroll Dollar',
+'Payroll Growth Assumption',
+'Total Benefits Paid Dollar')")}else{
+  
+  plan_id <- pl$id[pl$display_name == plan_name]
+  query <- paste("select * from pull_plan_data(",plan_id,")")
+}
+}
