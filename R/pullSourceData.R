c963328eb0c9e6e772e379e79911cdeda9597062
Anil Niraula <niraulaanil@gmail.com>
2020-08-05 19:57:48 -0400

    Updated pullSourceData

--- R/pullSourceData.R
+++ R/pullSourceData.R
@@ -2,15 +2,16 @@
 #'
 #' @param plan_name A string enclosed in quotation marks containing a plan name as it is listed in the Reason pension database.
 #' @param pl A datafram containing the list of plan names, states, and ids in the form produced by the planList() function.
+#' @param fy A fiscal year starting point.
 #' @return A wide data frame with each year as a row and variables as columns.
 #' @export
 #' @importFrom rlang .data
 #' @examples
 #' \dontrun{
 #' pullData(pl)
-#' pullSourceData(pl, "Kansas Public Employees' Retirement System")
+#' pullSourceData(pl, "Kansas Public Employees' Retirement System", fy)
 #' }
-pullSourceData <- function(pl, plan_name){
+pullSourceData <- function(pl, plan_name, fy){
   #dw <- get("dw")
   con <- RPostgres::dbConnect(
     RPostgres::Postgres(),
@@ -36,8 +37,9 @@ pullSourceData <- function(pl, plan_name){
   all_data %>%
     dplyr::group_by_at(dplyr::vars(-.data$attribute_value)) %>%  # group by everything other than the value column.
     dplyr::mutate(row_id = 1:dplyr::n()) %>%
+    dplyr::filter(year >= fy) %>%
     dplyr::ungroup() %>%  # build group index
     tidyr::spread(.data$attribute_name, .data$attribute_value, convert = TRUE) %>%    # spread
     dplyr::select(-.data$row_id) %>%  # drop the index
     janitor::clean_names()
-  }
+}
