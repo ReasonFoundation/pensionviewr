af6286a1d5af25caa8aa13d17789984bead2ab9c
Anil Niraula <niraulaanil@gmail.com>
2020-11-26 19:41:41 -0500

    Updt masterView

--- R/masterView.R
+++ R/masterView.R
@@ -14,13 +14,12 @@
 masterView <- function(source = NULL, expand = FALSE){
   con <- RPostgres::dbConnect(
     RPostgres::Postgres(),
-    dbname = trimws(dw$path),
-    host = dw$hostname,
-    port = dw$port,
-    user = dw$username,
-    password = dw$password,
-    sslmode = "require"
-  )
+    dbname = "d629vjn37pbl3l",
+    host = "ec2-3-209-200-73.compute-1.amazonaws.com",
+    port = 5432,
+    user = "reason_readonly",
+    password = "p88088bd28ea68027ee96c65996f7ea3b56db0e27d7c9928c05edc6c23ef2bc27",
+    sslmode = "require")
   
   query <- paste("select * from", 
                  if(isTRUE(expand)){paste("plan_attribute")}
@@ -34,18 +33,18 @@ masterView <- function(source = NULL, expand = FALSE){
   RPostgres::dbClearResult(result)
   RPostgres::dbDisconnect(con)
   
-  all_data <- data.table(all_data)
+  all_data <- data.frame(all_data)
   
   if(is.null(source)){
-    all_data <- data.table(all_data)
+    all_data <- data.frame(all_data)
     
   }else if(!is.null(source) & !isTRUE(expand)){
-    all_data <- all_data[ data_source_name == source]
+    all_data <- data.frame(all_data %>% filter(data_source_name == source))
     
   }else if(!is.null(source) & source == "Reason" & isTRUE(expand)){
-    all_data <- all_data[ data_source_id == 3]}
+    all_data <- data.frame(all_data %>% filter(data_source_id == 3))}
   
-  else{all_data <- data.table(all_data)
+  else{all_data <- data.frame(all_data)
   }
   
 }
