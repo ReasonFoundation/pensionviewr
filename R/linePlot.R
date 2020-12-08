94ffa3baa2a7af8b15e1e974b2632784dfc7874b
Anil Niraula <niraulaanil@gmail.com>
2020-12-07 20:28:45 -0500

    Updt linePlot()

--- R/linePlot.R
+++ R/linePlot.R
@@ -27,27 +27,29 @@ linePlot <- function (data, yaxisMin = 0, yaxisMax = NULL, yaxisSeq = 5,
 {
   reasontheme::set_reason_theme(style = "slide")
   
-  data <- data.frame(data) %>% dplyr::mutate_all(dplyr::funs(as.numeric))
-  
-  #Geomean
-  if (sum(data$return_1yr) > 0) {
+  if (sum(!is.na(PERSI.data$return_1yr)) > 0) {
+    
     geomean <- function(x) {
       x <- as.vector(na.omit(x))
       x <- x + 1
       exp(mean(log(x))) - 1
     }
+    
     returns <- as.numeric(data$return_1yr)
     nyear <- 10
     rolling <- geomean(returns[1:nyear])
     n <- length(na.omit(returns)) - nyear
     for (i in 1:n) {
-      rolling <- rbind(rolling, geomean(returns[(i + 1):(i + nyear)]))
+      rolling <- rbind(rolling, geomean(returns[(i + 1):(i + 
+                                                           nyear)]))
     }
-    data <- data.table(data)
+    
+    data <- data.frame(data) %>% dplyr::mutate_all(dplyr::funs(as.numeric))
     rolling <- data.table(rolling)
     data <- data.table(rbind.fill(rolling, data))
     x <- data[!is.na(return_1yr), .N]
-    data[(x + 1):(x + rolling[, .N])]$V1 <- data[(1:rolling[, .N])]$V1
+    data[(x + 1):(x + rolling[, .N])]$V1 <- data[(1:rolling[, 
+                                                            .N])]$V1
     data <- data[!(1:rolling[, .N])]
     data$year <- as.numeric(data$year)
   }
@@ -55,7 +57,8 @@ linePlot <- function (data, yaxisMin = 0, yaxisMax = NULL, yaxisSeq = 5,
     NULL
   }
   if (sum(data$return_1yr) > 0) {
-    data <- data %>% select(year, return_1yr, ava_return, arr, V1)
+    data <- data %>% select(year, return_1yr, ava_return, 
+                            arr, V1)
   }
   else {
     NULL
@@ -71,18 +74,13 @@ linePlot <- function (data, yaxisMin = 0, yaxisMax = NULL, yaxisSeq = 5,
   }, if (!is_null(lab5)) {
     paste(lab5)
   })
-  #Melting data from wide to long format
   graph <- data.table(melt(data, id.vars = "year"))
-  
   lineColors <- c(palette_reason$Orange, palette_reason$Yellow, 
                   palette_reason$SatBlue, palette_reason$LightGrey)
   options(repr.plot.width = 1, repr.plot.height = 0.75)
-  
-  #Graphing
   ggplot2::ggplot(graph, ggplot2::aes(x = year, y = yaxisScale * 
-  value, group = variable)) + 
-    ggplot2::geom_line(ggplot2::aes(colour = str_wrap(factor(variable), 
-                str)), size = 1.5) + ggplot2::geom_hline(yintercept = 0, 
+                                        value, group = variable)) + ggplot2::geom_line(ggplot2::aes(colour = str_wrap(factor(variable), 
+                                                                                                                      str)), size = 1.5) + ggplot2::geom_hline(yintercept = 0, 
                                                                                                                                                                color = "black") + ggplot2::scale_colour_manual(values = lineColors) + 
     ggplot2::scale_y_continuous(breaks = seq(yaxisMin, if (!is.null(yaxisMax)) {
       yaxisMax
@@ -104,7 +102,7 @@ linePlot <- function (data, yaxisMin = 0, yaxisMax = NULL, yaxisSeq = 5,
         paste0(format, round(b, 0))
       }
     }, expand = c(0, 0)) + ggplot2::scale_x_continuous(breaks = seq(min(graph$year), 
-       max(graph$year), by = 2), expand = c(0, 0)) + labs(x = element_blank(), 
+                                                                    max(graph$year), by = 2), expand = c(0, 0)) + labs(x = element_blank(), 
                                                                                                                        y = labelY) + theme(legend.text = element_text(size = 13)) + 
     theme(legend.direction = "vertical", legend.box = "horizontal", 
           legend.position = c(0.33, 0.09))
