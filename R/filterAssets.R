13a91ca5473968dcd98d19a95e71403e45c67821
Anil Niraula <niraulaanil@gmail.com>
2021-04-24 09:36:54 -0400

    Adding pullAssetData() & filterAssets()

---
+++ R/filterAssets.R
@@ -0,0 +1,150 @@
+#' Filter Asset data
+#'
+#' @param assets data frame or data.table containing PPD's asset allocation data.
+#' @param fy Numeric parameter containing a starting fiscal year as it is listed in the Reason pension database.
+#' @param plan name of state-managed pension plan to filter end data for.
+#' @return A wide data frame with each year as a row and variables as columns.
+#' @export
+#' @importFrom rlang .data
+#' @examples
+#' \dontrun{
+#' filterAssets(assets, fy = 2001, plan = NULL)
+#' }
+#' @author Anil Niraula <anil.niraula@reason.org>
+
+filterAssets <- function(assets, fy = 2001, plan = NULL){
+  
+  assets[,5:74]  <- assets[,5:74] %>% mutate_all(as.numeric)
+  #View(assets[,75])## TOTAL
+  #View(reason.data$unfunded_actuarially_accrued_liabilities_dollar)
+  
+  ## Convert NA's to 0
+  assets <- assets %>%
+    replace(is.na(.), 0)
+  
+  #y <- as.numeric(length(assets$total))
+  
+  #for (i in (1: y)){
+  #  assets$total[i] <- sum(assets[i,5:75])  
+  #}
+  
+  assets$year <- as.numeric(assets$year)
+  
+  #for (i in (1: y)){
+  #  assets$total[i] <- sum(assets[i,5:75])  
+  #}
+  #View(colnames(assets))
+  
+  equity <- data.frame(assets %>% select(contains("equity"), 
+                                         -contains("private"),
+                                         -contains("private_debt"),
+                                         -contains("alternative")))
+  #View(equity)
+  fixed <- data.frame(assets %>% select(contains("fixed"),
+                                        contains("treasury"),
+                                        contains("inflation_linked_bonds"),
+                                        contains("corporate_bonds")))
+  
+  private.equity <- data.frame(assets %>% select(contains("private_equity")))
+  
+  real.estate <- data.frame(assets %>% select(contains("estate")))
+  
+  cash <- data.frame(assets %>% select(contains("short"),
+                                       contains("cash")))
+  
+  infrastructure <- data.frame(assets %>% select(contains("infrastructure")))
+  
+  hedge <- data.frame(assets %>% select(contains("hedge"),
+                                        -contains("equity")))
+  
+  x <- c(colnames(equity), 
+         colnames(fixed),
+         colnames(private.equity),
+         colnames(real.estate),
+         colnames(cash),
+         colnames(infrastructure),
+         colnames(hedge))
+  
+  other <- data.frame(assets %>% select(-contains(x),
+                                        -contains("year"),
+                                        -contains("plan_id"),
+                                        -contains("display_name"),
+                                        -contains("state"),
+                                        -contains("market")))
+  
+  
+  #View(other)
+  library(skimr)
+  #skim(other)
+  
+  #View(colnames(assets))
+  #View(equity)
+  
+  assets$private.equity <- 0
+  assets$real.estate <- 0
+  assets$hedge <- 0
+  assets$infrastructure <- 0
+  assets$other.alternatives <- 0
+  assets$equity <- 0
+  assets$fixed <- 0
+  assets$cash <- 0
+  
+  for(i in (1:length(assets$equity))){
+    
+    assets$private.equity[i] <- sum(private.equity[i,])
+    assets$real.estate[i] <- sum(real.estate[i,])
+    assets$hedge[i] <- sum(hedge[i,])
+    assets$infrastructure[i] <- sum(infrastructure[i,])
+    assets$other.alternatives[i] <- sum(other[i,])
+    assets$equity[i] <- sum(equity[i,])
+    assets$fixed[i] <- sum(fixed[i,])
+    assets$cash[i] <- sum(cash[i,])
+    
+  }
+  
+  
+  assets1 <- assets %>% filter(display_name == plan) %>% select(year, display_name, state, 
+                                                                private.equity,
+                                                                real.estate,
+                                                                hedge,
+                                                                infrastructure,
+                                                                other.alternatives,
+                                                                equity,
+                                                                fixed,
+                                                                cash)
+  
+  
+  assets1$total <- 0
+  #View(assets2) 
+  y <- as.numeric(length(assets1$total))
+  
+  assets1[,4:11]  <- assets1[,4:11] %>% mutate_all(as.numeric)
+  
+  for (i in (1: y)){
+    assets1$total[i] <- sum(assets1[i,4:11])  
+  }
+  #View(assets1)
+  ### calculate shortage to 100% allocation & assign 
+  ### 50% to Equities & 50% to Other Alternatives
+  assets1$equity <- as.numeric(assets1$equity)
+  assets1$other.alternatives <- as.numeric(assets1$other.alternatives)
+  assets1$total <- as.numeric(assets1$total)
+  #View(assets1$total)
+  
+  
+  assets1 <- assets1 %>% filter(year >= 2001) %>% select(-total, -display_name, -state)
+  #View(assets1)
+  colnames(assets1) <- c("year",
+                         "Private Equity",
+                         "Real Estate",
+                         "Hedge Funds",
+                         "Infrastructure",
+                         "Other Alternatives",
+                         "Total Equity",
+                         "Fixed Income",
+                         "Cash Equivalents"
+  ) 
+  assets1
+  
+  
+}
---
+++ R/pullAssetData.R
@@ -0,0 +1,118 @@
+#' pullAssetData
+#'
+#' @param fy starting fiscal year to filter data for
+#' @param mva = FALSE (Set to `TRUE` to also pull MVA for weighting)
+#' @export
+#' @examples
+#' \dontrun{
+#' pullAssetData(fy = 2001, mva = FALSE) 
+#' }
+#' @importFrom rlang .data
+#' @author Anil Niraula <anil.niraula@reason.org>
+
+pullAssetData <- function (fy, mva = FALSE) 
+{
+  
+  #dw <- get("dw")
+  con <- RPostgres::dbConnect(
+    RPostgres::Postgres(),
+    dbname = trimws(dw$path),
+    host = dw$hostname,
+    port = dw$port,
+    user = dw$username,
+    password = dw$password,
+    sslmode = "require"
+  )
+  
+  query <- paste("select * from pull_data_state_only()\nwhere year > '", 
+                 paste(fy - 1), "'\nand attribute_name in ('International Equity Actual Allocation',
+\n'Domestic Equity Actual Allocation',
+\n'Fixed Income + Alternatives Actual Allocation',
+\n'Real Estate Actual Allocation',
+\n'Domestic Fixed Income Actual Allocation',
+\n'International Fixed Income Actual Allocation',
+\n'Private Equity Actual Allocation',
+\n'Absolute Return/Value Fund Actual Allocation',
+\n'Farmland/Agriculture Actual Allocation',
+\n'High Yield Fixed Income Actual Allocation',
+\n'Fixed Income Actual Allocation',
+\n'Cash/Short-term Actual Allocation',
+\n'Inflation Linked Bonds/TIPS Actual Allocation',
+\n'Misc. Equity Actual Allocation',
+\n'Other” Actual Allocation',
+\n'Large Cap Equity Actual Allocation',
+\n'Micro Cap Equity Actual Allocation',
+\n'Multi-Asset Class/Diversified Actual Allocation',
+\n'REIT Actual Allocation',
+\n'Real Assets/Real Return Actual Allocation',
+\n'Credit Opportunities/Private Credit Actual Allocation',
+\n'GTAA/GAA/Global Macro Hedge Actual Allocation',
+\n'Risk Parity Actual Allocation',
+\n'Opportunistic Equity Alternative Actual Allocation',
+\n'Opportunistic Debt Alernatives Actual Allocation',
+\n'Emerging Fixed Income Actual Allocation',
+\n'Private Debt Actual Allocation',
+\n'Equity Securities Lending Actual Allocation',
+\n'Infrastructure Actual Allocation',
+\n'MLP/Limited Partnership Actual Allocation',
+\n'Investment Grade Fixed Income Actual Allocation',
+\n'Loans Actual Allocation',
+\n'Large Cap Domestic Equity Actual Allocation',
+\n'Small Cap Domestic Equity Actual Allocation',
+\n'Private Real Estate Actual Allocation',
+\n'Timberland Actual Allocation',
+\n'Hedge Funds Actual Allocation',
+\n'Fixed Income Mortgages/Securitized Debt Actual Allocation',
+\n'Inflation-Sensititive Actual Allocation',
+\n'Global Fixed Income Actual Allocation',
+\n'Private Placement Actual Allocation',
+\n'Global Equity Actual Allocation',
+\n'Core Fixed Income Actual Allocation',
+\n'Opportunity/Opportunistic Actual Allocation',
+\n'Commodities Actual Allocation',
+\n'Core Real Estate Actual Allocation',
+\n'Emerging International Equity Actual Allocation',
+\n'Developed International Equity Actual Allocation',
+\n'Relative Return/Value Fund Actual Allocation',
+\n'US Treasury Actual Allocation',
+\n'Covered Call Actual Allocation',
+\n'Natural Resources Actual Allocation',
+\n'Non-Core Fixed Income Actual Allocation',
+\n'Opportunistic Fixed Income Actual Allocation',
+\n'Corporate Bonds Actual Allocation',
+\n'Misc. Alternatives Actual Allocation',
+\n'Mid Cap Domestic Equity Actual Allocation',
+\n'Small Cap Equity” Actual Allocation',
+\n'Equity Hedge Actual Allocation',
+\n'Non-Core Real Estate Actual Allocation',
+\n'Fixed Income Funds of Funds Actual Allocation',
+\n'Value Added Fixed Income Actual Allocation',
+\n'Passive International Equity Actual Allocation',
+\n'Active International Equity Actual Allocation',
+\n'Core Equity Actual Allocation',
+\n'Global Growth Equity Actual Allocation',
+\n'Triple Net Lease Actual Allocation',
+\n'Distressed Debt Actual Allocation',
+\n'Distressed Lending Actual Allocation',
+\n'GIPS Actual Allocation',
+\n'Misc. Equity and Fixed Income Blends Actual Allocation',
+\n'Opportunistic Equity Actual Allocation',
+\n'Social Responsibility Equity Actual Allocation',
+\n'Nominal Fixed Income Actual Allocation',
+\n'Convertible Fixed Income Actual Allocation',
+\n'Fixed Income ETI Actual Allocation',
+\n'Structured Fixed Income Actual Allocation',
+\n'Fixed Income Below Investment Grade Actual Allocation',
+\n'Fixed Income + Cash Actual Allocation'",if(isTRUE(mva)){paste(",\n'Market Value of Assets Dollar'")},
+                 ")")
+  result <- RPostgres::dbSendQuery(con, query)
+  all_data <- RPostgres::dbFetch(result) %>% janitor::clean_names()
+  RPostgres::dbClearResult(result)
+  RPostgres::dbDisconnect(con)
+  
+  all_data %>% dplyr::group_by_at(dplyr::vars(-.data$attribute_value)) %>% 
+    dplyr::mutate(row_id = 1:dplyr::n()) %>% dplyr::ungroup() %>% 
+    tidyr::pivot_wider(names_from = attribute_name, values_from = attribute_value) %>% 
+    dplyr::select(-.data$row_id) %>% dplyr::arrange(display_name, 
+                                                    year) %>% janitor::clean_names()
+}
